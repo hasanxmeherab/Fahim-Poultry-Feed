bef05c4374999a7db5d9a0d8120bd4c2f69b649b
Meherab Hasan Fahim <fahim2305341670@diu.edu.bd>
2025-10-11 00:31:31 +0600

    Update userRoutes.js

--- Fahim-Poultry-Feed-Backend/routes/userRoutes.js
+++ Fahim-Poultry-Feed-Backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/userController');
 
-router.post('/register', registerUser); // For one-time use
+//router.post('/register', registerUser); // For one-time use
 router.post('/login', loginUser);
 
 module.exports = router;
